bc4ff7472b2a61b130fa60294c768645dfab5cd9
Alvi <mohalviridho@gmail.com>
2024-04-30 13:53:37 +0700

    feat:transaksi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -12,9 +12,29 @@ class TransaksiController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $nama_barang = $request->input('nama_barang');
+        $tanggal_transaksi = $request->input('tanggal_transaksi');
+
+        $query = Transaksi::query();
+
+        if ($nama_barang) {
+            $query->whereHas('barang', function ($q) use ($nama_barang) {
+                $q->where('nama_barang', 'like', "%$nama_barang%");
+            });
+        }
+
+        if ($tanggal_transaksi) {
+            $query->whereDate('tanggal_transaksi', $tanggal_transaksi);
+        }
+
+        $transaksis = $query->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $transaksis,
+        ]);
     }
 
     /**
@@ -50,7 +70,11 @@ class TransaksiController extends Controller
      */
     public function show(Transaksi $transaksi)
     {
-        //
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Barang',
+            'data' => $transaksi
+        ]);
     }
 
     /**
@@ -64,16 +88,45 @@ class TransaksiController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Transaksi $transaksi)
+    public function update(Request $request, $id)
     {
-        //
+        $transaksi = Transaksi::findOrFail($id);
+        $oldQuantity = $transaksi->jumlah_terjual;
+
+        $transaksi->update($request->all());
+
+        // Menghitung selisih jumlah terjual baru dengan jumlah terjual lama
+        $diff = $request->jumlah_terjual - $oldQuantity;
+
+        // Memperbarui stok barang berdasarkan selisih jumlah terjual
+        $barang = Barang::findOrFail($transaksi->barang_id);
+        $barang->stok -= $diff; // Perubahan disesuaikan berdasarkan perbedaan jumlah terjual
+        $barang->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaksi updated successfully',
+            'data' => $transaksi
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Transaksi $transaksi)
+    public function destroy($id)
     {
-        //
+        $transaksi = Transaksi::findOrFail($id);
+
+        // Mengembalikan jumlah terjual ke stok barang sebelum menghapus transaksi
+        $barang = Barang::findOrFail($transaksi->barang_id);
+        $barang->stok += $transaksi->jumlah_terjual;
+        $barang->save();
+
+        $transaksi->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaksi deleted successfully',
+        ]);
     }
 }
